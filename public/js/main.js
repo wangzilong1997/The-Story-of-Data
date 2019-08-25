db19a7c37031715b0240e06f396c34aa7ceb4bcb
wangzilong1997 <1321787581@qq.com>
2019-08-25 16:34:38 +0800

    主页面装修

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,13 @@
-function header(){
-	const a = document.getElementsByClassName("otherheader");
-	a[0].attributes.style.nodeValue = " "
-}
-function headerout(){
-	const a = document.getElementsByClassName("otherheader");
-	a[0].attributes.style.nodeValue = "background-color:#fbeeca";
-}
+let a = 0
+$("#qqortim").change(() => {
+	console.log("change")
+	console.log(a)
+	a++
+	if( a%2 == 1){
+		$("#bili")[0].innerHTML = '<iframe src="//player.bilibili.com/player.html?aid=52425230&cid=91750540&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="370" height="278"> </iframe>'
+		console.log($("#bili")[0].innerHTML)
+
+	}else{
+		$("#bili")[0].innerHTML = '<iframe src="//player.bilibili.com/player.html?aid=52425371&cid=91750957&page=1 "scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="370" height="278"> </iframe>'
+	}
+})
